f4e6e0927082623f413c8222f4dd0a28fb93418f
Kevin Y. Kim <kevinyckim33@gmail.com>
2021-02-11 18:43:33 -0500

    Finish 75 of 279: Concurrent Compilation and Execution with Nodemon and Concurrently

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+console.log('hi there')
+// tsc index.ts ==> index.js
+
+//  tsc --init  ==> tsconfig.json
+
+// change rootDir and outDir
+
+// tsc -w /* watch the src directory and spit out compiled version into build */
+
+const logSomething = () => {
+    console.log('something');
+    console.log('sup!!!')
+}
+
+// node build/index.js
+// npm install nodemon concurrently
+// nodemon: is going to allow us to rerun node anytime a file changes inside of our project. 
+// so nodemon is going to be resp for executing our code once it has been compiled.
+// concurrently: help us run multiple scripts at the same time; in particular, run script to start up typescript compiler, 
+// #2 to run nodemon to execute that code
+
+/*
+  "scripts": {
+    "start:build": "tsc -w",
+    "start:run": "nodemon build/index.js",
+    "start": "concurrently npm:start:*"
+  },
+
+  start:build ==> watches rootDir in tsconfig.json "./src"; anytime there's a change, converts it to .js files inside outDir "./build"
+  start:run ==> anytime there is a change in build/index.js, it's going to execude node index.js
+  start: concurrently fires anything that has start:whatever
+*/
