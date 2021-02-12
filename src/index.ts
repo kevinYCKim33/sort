3e3cfb86731bb33c7c870dfa41e8aec95086c59a
Kevin Y. Kim <kevinyckim33@gmail.com>
2021-02-11 22:13:13 -0500

    Finish 81 of 279: Type Guards

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-console.log('hi there')
 // tsc index.ts ==> index.js
 
 //  tsc --init  ==> tsconfig.json
@@ -7,16 +6,11 @@ console.log('hi there')
 
 // tsc -w /* watch the src directory and spit out compiled version into build */
 
-const logSomething = () => {
-    console.log('something');
-    console.log('sup!!!')
-}
-
 // node build/index.js
 // npm install nodemon concurrently
-// nodemon: is going to allow us to rerun node anytime a file changes inside of our project. 
+// nodemon: is going to allow us to rerun node anytime a file changes inside of our project.
 // so nodemon is going to be resp for executing our code once it has been compiled.
-// concurrently: help us run multiple scripts at the same time; in particular, run script to start up typescript compiler, 
+// concurrently: help us run multiple scripts at the same time; in particular, run script to start up typescript compiler,
 // #2 to run nodemon to execute that code
 
 /*
@@ -30,3 +24,49 @@ const logSomething = () => {
   start:run ==> anytime there is a change in build/index.js, it's going to execude node index.js
   start: concurrently fires anything that has start:whatever
 */
+
+// class Sorter {
+//   collection: number[];
+
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+
+// EQUIVALENT TO ABOVE:
+class Sorter {
+  constructor(public collection: number[] | string) {}
+
+  sort(): void {
+    const { length } = this.collection;
+    // All of this only works if collection is number[]
+    // If collection is an array of numbers
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // Type Guard for Arrays
+        if (this.collection instanceof Array) {
+          // TS now knows inside this block, this.collection is an Array
+          // suggests more Array methods
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+        // Type Guard for Strings
+        if (typeof this.collection === "string") {
+          // TS now knows inside this block, this.collection is a string
+          // suggests more string methods
+        }
+      }
+    }
+
+    // Only going to work if collection is a string
+    // If a collection is a string, do this logic instead:
+    //
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
