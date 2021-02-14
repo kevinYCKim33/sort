16be53b54455a09d064b96f06f76d2a94b576106
Kevin <kevinyckim33@gmail.com>
2021-02-13 20:42:58 -0500

    bare minimum LinkedList set up

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -25,6 +25,7 @@ export class LinkedList {
     tail.next = node; // add the node you just created to the end
   }
 
+  // length is asked for in this.collection.length
   get length(): number {
     if (!this.head) {
       return 0;
@@ -40,6 +41,7 @@ export class LinkedList {
     return length;
   }
 
+  // at is needed for compare() and swap()
   at(index: number): Node {
     if (!this.head) {
       throw new Error("Index out of bounds");
@@ -59,4 +61,40 @@ export class LinkedList {
 
     throw new Error("Index out of bounds");
   }
+
+  // once at() implemented, it's just almost the same;
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error("");
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  // kind of cheating...
+  // not actually swapping linked list nodes, but the values at each of the nodes
+  // uses the at() method
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  // doesn't really need anything for sorting linked list; it's just there more for as a check;
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
 }
+
+const linkedList = new LinkedList();
